85faa1ee13ba2d72ac45ed2fd52b72eed996a749
user <user@DESKTOP-1FJ2JCI>
2026-07-30 09:06:05 +0900

    feat: Spring Boot 기본 프로젝트 구조 및 환경 설정 완료

---
+++ ending_credit/src/test/java/com/endit/EndingCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndingCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
